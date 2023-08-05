cc2ca3074eaac641b3272fd31d6b3bec5a652170
dbrjsdml <dbrjsdml@naver.com>
2023-08-05 20:57:20 +0900

    타임리프 환경설정 완료

---
+++ tymeleaf-basic/src/main/java/hello/tymeleafbasic/TymeleafBasicApplication.java
@@ -0,0 +1,12 @@
+package hello.tymeleafbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TymeleafBasicApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TymeleafBasicApplication.class, args);
+	}
+
+}
